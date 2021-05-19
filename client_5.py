c50d0d2a864bcf5a27ee53fa49761dd38a34304e
Kelvin Likollari <55785978+likolk@users.noreply.github.com>
2021-05-19 16:25:12 +0200

    .

---
+++ client_5.py
@@ -0,0 +1,21 @@
+import socket
+
+
+FORMAT = "UTF-8"
+
+
+
+SERVER = "10.40.0.46"
+
+
+PORT = 9994
+
+
+ADDRESS = (SERVER, PORT)
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+client.connect(ADDRESS)
+
