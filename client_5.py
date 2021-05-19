7fd859b0998f48482f154c678af994d6f0f2eee2
Kelvin Likollari <55785978+likolk@users.noreply.github.com>
2021-05-19 17:52:54 +0200

    code drafts

--- client_5.py
+++ client_5.py
@@ -1,3 +1,31 @@
+
+
+
+
+
+
+'''
+Suppose there is exactly one switch between a sending host and a 
+receiving host. The transmission rates between the sending host 
+and the switch and between the switch and the receiving host are R1 and R2, respectively. 
+Assuming that the switch uses store-and-forward packet switching, what is the total end-to-end 
+delay to send a packet of length L?
+
+Answer (before getting the values from the server):
+General Idea: 
+We have R1 and R2 that are the transmission rates between the sending host and the switch, 
+and the transmission rate between the switch and the receiving host respectively.
+L = length of packet.
+Therefore, given the values that we obtain from the server, we are going to compute:
+L = L / R1   +    L / R2 
+
+
+
+
+'''
+
+
+
 import socket
 
 
